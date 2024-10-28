c5f761cb00a383d2c2b7d8fe6c19c02f7c617427
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-10-28 17:14:18 +0800

    Create tally of registration hours

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,7 +3,6 @@ require 'google/apis/civicinfo_v2'
 require 'erb'
 require 'time'
 
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -16,9 +15,9 @@ def legislator_by_zipcode(zip)
     civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representative by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
@@ -59,17 +58,32 @@ def create_date_obj(date_string)
   Time.new(date[2].rjust(4, '20'), date[0], date[1], time[0], time[1])
 end
 
+def peak_hours(dates)
+  tally = tally_hours(dates)
+  p tally
+end
+
+def tally_hours(dates)
+  hours = []
+  dates.each do |date|
+    hours << date.hour
+  end
+  hours.tally
+end
+
 puts 'Event Manager Initialized!'
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+registration_dates = []
+
 if File.exist? 'event_attendees.csv'
   contents = CSV.open(
-    'event_attendees.csv', 
-    headers: true, 
+    'event_attendees.csv',
+    headers: true,
     header_converters: :symbol
-    )
+  )
   contents.each do |row|
     id = row[0]
     name = row[:first_name]
@@ -80,13 +94,14 @@ if File.exist? 'event_attendees.csv'
 
     num = clean_number(phone)
 
-    date_time = create_date_obj(row[:regdate])
+    registration_dates << create_date_obj(row[:regdate])
 
     # legislators = legislator_by_zipcode(zipcode)
 
     # form_letter = erb_template.result(binding)
-    
-    # save_thank_you_letter(id, form_letter)
 
+    # save_thank_you_letter(id, form_letter)
   end
 end
+
+peak_hours(registration_dates)
